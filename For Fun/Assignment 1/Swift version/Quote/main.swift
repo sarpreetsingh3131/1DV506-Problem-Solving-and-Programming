d66a18fa8623e5c78033359403b2f7be386dbc52
sarpreetsingh3131 <sarpreetsinghbuttar@outlook.com>
2017-07-03 17:54:01 +0200

    updates

--- For Fun/Assignment 1/Swift version/Quote/main.swift
+++
@@ -1,9 +0,0 @@
-//  Created by Sarpreet Singh on 2016-12-17.
-//  Copyright © 2016 Sarpreet Singh. All rights reserved.
-//
-
-import Foundation
-
-print("Write a line of text: ", terminator: "")
-let input = readLine()!
-print("\"\(input)\"")
